60aa57eb6fe16ddaa4dca0f624da5e749494a05d
ThiwankaS <thivankas@outlook.com>
2026-01-31 06:26:04 +0200

    spin up a simple srever

---
+++ cmd/api.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string // address of the server
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn string
+}
+
+// mount
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("all good...\n"))
+	})
+
+	return r
+}
+
+// run
+func (app *application) run(h http.Handler) error {
+
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      h,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("Server started to listen on address %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
