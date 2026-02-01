2e38b20bb2e3028e79795269d35d63562fdc18ac
ThiwankaS <thivankas@outlook.com>
2026-02-01 08:26:36 +0200

    configure db connection and migration

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
+
+	"github.com/ThiwankaS/ecom-go-api-project/internal/env"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+
+	ctx := context.Background()
+
 	cfg := config{
 		addr: ":8080",
-		db:   dbConfig{},
+		db: dbConfig{
+			dsn: env.GetString("GOOSE_DBSTRING",
+				"host=localhost user=postgres password=postgres dbname=ecom sslmode=disable"),
+		},
 	}
+
+	// Structured logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	// Database connection
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(ctx)
+
+	logger.Info("connected to database", "dsn", cfg.db.dsn)
 
 	api := application{
 		config: cfg,
 	}
-
-	// structured logger
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	slog.SetDefault(logger)
 
 	if err := api.run(api.mount()); err != nil {
 		slog.Error("Server failed to start %s", "error", err)
